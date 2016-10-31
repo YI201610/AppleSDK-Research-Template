03beef232e4200341f95684432e74a30786b2adc
Yuji Imamura <imamura@imarketsceneviewer.com>
2016-11-01 00:05:20 +0900

    iOSアプリケーションを追加

---
+++ AppleSDKTips/AppleSDKTips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleSDKTips
+//
+//  Created by 今村裕治 on 2016/10/31.
+//  Copyright © 2016年 Yuji Imamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
