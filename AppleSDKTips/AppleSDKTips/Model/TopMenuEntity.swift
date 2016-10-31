e8b36e7e37b6776d8a120ff01a852ed8b236aeeb
Yuji Imamura <imamura@imarketsceneviewer.com>
2016-11-01 00:13:33 +0900

    topMenuのエンティティを追加

---
+++ AppleSDKTips/AppleSDKTips/Model/TopMenuEntity.swift
@@ -0,0 +1,22 @@
+//
+//  TopMenuEntity.swift
+//  AppleSDKTips
+//
+//  Created by 今村裕治 on 2016/11/01.
+//  Copyright © 2016年 Yuji Imamura. All rights reserved.
+//
+
+import UIKit
+
+/** Topメニューに表示する各検証項目のエンティティ */
+class TopMenuEntity: NSObject {
+
+    var sectionNameString: String?
+    
+    var titleString: String?
+    
+    var viewControllerNameString: String?
+    
+    var windowControllerNameString: String?
+    
+}
---
+++ AppleSDKTips/AppleSDKTips/ModelController/TopMenuModelController.swift
@@ -0,0 +1,15 @@
+//
+//  TopMenuModelController.swift
+//  AppleSDKTips
+//
+//  Created by 今村裕治 on 2016/11/01.
+//  Copyright © 2016年 Yuji Imamura. All rights reserved.
+//
+
+import UIKit
+
+class TopMenuModelController: NSObject {
+
+    
+    
+}
