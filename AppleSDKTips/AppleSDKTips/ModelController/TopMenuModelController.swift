e215d546407858b2adabb8cac374dc69a1220490
Yuji Imamura <imamura@imarketsceneviewer.com>
2016-11-03 00:36:38 +0900

    - メニューに関するモデルコントローラーを追加 - テストコードを追加

--- AppleSDKTips/AppleSDKTips/ModelController/TopMenuModelController.swift
+++ AppleSDKTips/AppleSDKTips/ModelController/TopMenuModelController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class TopMenuModelController: NSObject {
 
+    init(plistName: String) {
+        
+    }
+
+    func numberOfSection() -> Int {
+        return 0
+    }
+
+    func numberOfItem(sectionName: String) -> Int {
+        return 0
+    }
+    
+    
+    func sectionName(index: Int) -> String {
+        return ""
+    }
+ 
+    
+    func itemForSection(sectionName: String, index: Int) -> TopMenuEntity {
+     
+        return TopMenuEntity()
+    }
     
     
 }
