eb4c1db50f8f116ace30a435977bbb9d045138ac
Yuji Imamura <imamura@imarketsceneviewer.com>
2016-11-03 23:20:06 +0900

    テストコードをの整備

--- AppleSDKTips/AppleSDKTips/ViewController/TopMenuViewController.swift
+++ AppleSDKTips/AppleSDKTips/ViewController/TopMenuViewController.swift
@@ -10,6 +10,15 @@ class TopMenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        let testBundle = Bundle(for: type(of: self))
+        let plistPath = testBundle.path(forResource: "topMenu", ofType: "plist")
+        
+        guard let plistName = plistPath else {
+            return
+        }
+
+        print(plistName)
     }
 
 }
--- AppleSDKTips/AppleSDKTipsTests/TopMenuModelControllerTests.swift
+++ AppleSDKTips/AppleSDKTipsTests/TopMenuModelControllerTests.swift
@@ -18,25 +18,98 @@ class TopMenuModelControllerTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+        let testBundle = Bundle(for: type(of: self))
+        let plistPath = testBundle.path(forResource: "topMenuTest", ofType: "plist")
         
-        controller = TopMenuModelController(plistName: "hoge.plist")
+        guard let plistName = plistPath else {
+            return
+        }
+        
+        controller = TopMenuModelController(plistName: plistName)
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+
+    func testThatControllerHasMenuSpec() {
+        
+        guard let model = controller else {
+            XCTAssert(false, "model controller作成失敗")
+            return
         }
+        
+        XCTAssertTrue(model.numberOfSection() > 0, "複数のセクションを持つこと")
     }
+    
+    /*
+     - (void)testThatControllerHasMenuSpec {
+     XCTAssertTrue([modelController numberOfSection] > 0, @"複数のセクションをもつこと");
+     }
+     
+     - (void) testThatControllerCanReturnSectionKeyString
+     {
+     NSInteger countOfSection = [modelController numberOfSection];
+     XCTAssertGreaterThan(countOfSection, 0, "必ず１つ以上のセクションをもつこと");
+     
+     for(int i = 0; i < countOfSection; i++){
+     NSString* sectionNameString = [modelController sectionNameStringWithIndex:i];
+     XCTAssertNotNil(sectionNameString, @"全てのセクションは名称をもつこと");
+     }
+     }
+     
+     /*!
+     @abstract セクションが保持するアイテム数を返すメソッドがあること
+     */
+     - (void) testThatControllerCanReturnCountOfItemsInSection
+     {
+     NSString* sectionNameString = @"WWW";
+     NSInteger countOfItem = [modelController numberOfItemForSection:sectionNameString];
+     XCTAssertGreaterThanOrEqual(countOfItem, 0, @"セクションが保持するアイテム数を取得することができる");
+     }
+     
+     /*!
+     @abstract  全てのセクションの項目数を取得することができること
+     */
+     - (void) testThatControllerCanReturnEachSectionCount
+     {
+     NSInteger countOfSection = [modelController numberOfSection];
+     for(int i = 0; i < countOfSection; i++){
+     NSString* sectionNameString = [modelController sectionNameStringWithIndex:i];
+     NSInteger countOfItem = [modelController numberOfItemForSection:sectionNameString];
+     
+     XCTAssertGreaterThanOrEqual(countOfItem, 0, @"全てのセクションの項目数を知ることができること");
+     }
+     }
+     
+     /*!
+     @abstract  コントローラーは、検証項目のエンティティを返すことができる
+     */
+     - (void) testThatControllerCanReturnItemForSectionAtIndex
+     {
+     NSInteger countOfSection = [modelController numberOfSection];
+     for(int i = 0; i < countOfSection; i++){
+     NSString* sectionNameString = [modelController sectionNameStringWithIndex:i];
+     NSInteger countOfItem = [modelController numberOfItemForSection:sectionNameString];
+     
+     for(int j = 0; j < countOfItem; j++){
+     MTMTopMenuEntity* entity = [modelController itemForSection:sectionNameString index:j];
+     XCTAssertNotNil(entity, @"モデルコントローラーは、指定されたセクション、指定された番号のエンティティを返すことができる: (%@:%@)", sectionNameString, entity);
+     }
+     }
+     
+     }
+     
+     
+     - (void)testPerformanceExample {
+     // This is an example of a performance test case.
+     [self measureBlock:^{
+     // Put the code you want to measure the time of here.
+     }];
+     }
+     
+     */
     
 }
