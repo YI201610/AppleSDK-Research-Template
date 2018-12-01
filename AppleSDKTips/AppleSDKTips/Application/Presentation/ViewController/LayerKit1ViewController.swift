92ab1354747d9a4520c886d0d5b7452896e8e04e
Yuji Imamura <YI201610@users.noreply.github.com>
2018-12-01 16:57:45 +0900

    ・不要なファイルを削除 ・いくつかの戻るボタンをunwindエグエに接続

--- AppleSDKTips/AppleSDKTips/Application/Presentation/ViewController/LayerKit1ViewController.swift
+++ AppleSDKTips/AppleSDKTips/Application/Presentation/ViewController/LayerKit1ViewController.swift
@@ -12,12 +12,6 @@ class LayerKit1ViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func unwindToTop(_ sender: UIStoryboardSegue) {
-        let sourceViewController = sender.source
-        // Use data from the view controller which initiated the unwind segue
-        sourceViewController.dismiss(animated: true, completion: {})
-    }
     
     @IBAction func didSelectTouchBarButton(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: {})
