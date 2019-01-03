1ff8267839268ce04f8038a2a0d3c1e98a39af29
Yuji Imamura <YI201610@users.noreply.github.com>
2019-01-03 16:47:38 +0900

    AV Foundation検証用のViewControllerを追加

---
+++ AppleSDKTips/AppleSDKTips/Application/Presentation/AVFoundation/BasicPlayback/BasicPlaybackViewController.m
@@ -0,0 +1,31 @@
+//
+//  BasicPlaybackViewController.m
+//  AppleSDKTips
+//
+//  Copyright © 2019 YI201610. All rights reserved.
+//
+
+#import "BasicPlaybackViewController.h"
+
+@interface BasicPlaybackViewController ()
+
+@end
+
+@implementation BasicPlaybackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ AppleSDKTips/AppleSDKTips/Application/Presentation/AVFoundation/BasicPlayback/README.md
@@ -0,0 +1,5 @@
+# 参考資料
+
+## Building a Basic Playback App
+
+- https://developer.apple.com/library/archive/documentation/AudioVideo/Conceptual/MediaPlaybackGuide/Contents/Resources/en.lproj/GettingStarted/GettingStarted.html
